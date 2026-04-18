d9a8878ffdaa25973eadbb04d6d9fdfbcb2190e3
Maxime Bisson <91592795+BY0D2C0D3@users.noreply.github.com>
2026-04-18 07:50:11 +0200

    Delete shell-master directory

--- shell-master/rofi/modes/clipboard.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -z "$ROFI_INFO" ]; then
-    cliphist list
-else
-    echo "$ROFI_INFO" | cliphist decode | wl-copy
-fi
--- shell-master/rofi/scripts/clipboard.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cliphist list \
-  | rofi -dmenu -i -p "Clipboard" \
-  | cliphist decode \
-  | wl-copy
